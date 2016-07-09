8e25d50e2e4a5c545022eb8b905c69ce0e3d5c81
arsh23 <programmer.arsh@gmail.com>
2016-07-10 04:30:19 +0530

    basic login implemented

--- ServerCode/Python/app.py
+++ ServerCode/Python/app.py
@@ -1,19 +1,21 @@
 from flask import Flask, request, session, g, redirect, url_for, render_template, jsonify
 import json
 import os
+from flask_bcrypt import Bcrypt
 # from flask.ext.cors import CORS
 from pymongo import MongoClient
 
 def connect():
-    connection = MongoClient("ds023118.mlab.com",23118)
-    handle = connection["users"]
-    handle.authenticate("admin","1234")
+    connection = MongoClient("ds017155.mlab.com",17155)
+    handle = connection["login_data"]
+    handle.authenticate("Yo@admin","1234")
     return handle
 
 app = Flask(__name__)
 # CORS(app)
+bcrypt = Bcrypt(app)
 db = connect()
-
+collection = 'login_user_data'
 @app.route('/')
 def home():
     return 'Yo'
@@ -22,8 +24,11 @@ def home():
 @app.route('/login', methods = ['POST'])
 def login():
     user_data = request.json
-    print user_data
-
+    stored_data = db[collection].find({"username": user_data['username']})[0]
+    if bcrypt.check_password_hash(stored_data['hashed_password'], user_data['password']) == True:
+        return jsonify({'status':'Logged In'})
+    else:
+        return jsonify({'status':'Wrong Password'})
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
