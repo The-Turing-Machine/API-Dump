9cc1d7266c2bbe0041ebe56635def5d2e84cbdb6
ayush <ayushkumarsingh97@gmail.com>
2016-07-09 21:44:55 +0530

    user added

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ import sys
 import pymongo
 from pymongo import MongoClient
 import json
-connection = MongoClient("ds017175.mlab.com", 17175)
-db = connection["apidump"]
+
+connection = MongoClient("ds017205.mlab.com",17205)
+db = connection["my_widgets"]
 # MongoLab has user authentication
-db.authenticate("admin", "1234")
+db.authenticate("ayush", "ayush")
+
+widgets = db.widgets
+
+json_data = open("widget.json")
+new_widget = json.load(json_data)
+widgets.insert(new_widget)
