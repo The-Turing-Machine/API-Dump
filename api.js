98ac59fbedc234bebd39c09d2ede99f186418fe5
thinker3197 <ashishgupta.3197@gmail.com>
2016-07-09 18:43:09 +0530

    Update lib structyre

---
+++ api.js
@@ -0,0 +1,33 @@
+(function($) {
+        'use strict';
+        var loginApi = function(elem, options) {
+            var ctrl = $(this),
+                el = $(elem);
+
+                ctrl.checkElem = function() {
+                    if(el.)
+                }
+
+            ctrl.setTemplate = function() {
+
+            }
+
+            ctrl.getFormVal = function() {
+
+            }
+        };
+
+
+        $.fn.api = function(options) {
+            return this.each(function() {
+                var apis = new loginApi($(this), options);
+            });
+        };
+
+        $fn.api.default = {
+
+        };
+
+    }
+
+})(jQuery);
