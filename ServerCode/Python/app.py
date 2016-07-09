c2fac65f32672f74c2e6e8277c746bd4a1b31715
arsh23 <programmer.arsh@gmail.com>
2016-07-09 22:51:15 +0530

    moved app.py

--- ServerCode/Python/app.py
+++ ServerCode/Python/app.py
@@ -18,6 +18,13 @@ db = connect()
 def home():
     return 'Yo'
 
+
+@app.route('/login', methods = ['POST'])
+def login():
+    user_data = request.json
+    print user_data
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
