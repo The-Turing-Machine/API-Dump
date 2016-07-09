b4f7786c7c91d79a88303206c807e2509cb959ad
thinker3197 <ashishgupta.3197@gmail.com>
2016-07-09 19:02:05 +0530

    Add hash function

--- api.js
+++ api.js
@@ -3,20 +3,41 @@
         var loginApi = function(elem, options) {
             var ctrl = $(this),
                 el = $(elem);
+            settings = $.extend({}, $.fn.api.default, options);
 
-                ctrl.checkElem = function() {
-                    if(el.)
-                }
+            ctrl.checkElem = function() {
+
+            };
 
             ctrl.setTemplate = function() {
 
-            }
+            };
 
             ctrl.getFormVal = function() {
 
+            };
+
+            ctrl.hashPassword = function() {
+                var password = elem.find('input');
+
+                var hashedPassword =
             }
         };
 
+        String.prototype.hashCode = function() {
+            var hash = 0,
+               	i,
+               	chr,
+               	len;
+
+            if (this.length === 0) return hash;
+            for (i = 0, len = this.length; i < len; i++) {
+                chr = this.charCodeAt(i);
+                hash = ((hash << 5) - hash) + chr;
+                hash |= 0;
+            }
+            return hash;
+        };
 
         $.fn.api = function(options) {
             return this.each(function() {
