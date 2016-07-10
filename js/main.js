fccb30e2c52112034841b0c134f8669ff2994d93
thinker3197 <ashishgupta.3197@gmail.com>
2016-07-10 08:29:45 +0530

    Major chnages

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function changeNav() {
+    $(window).on('scroll', function() {
+        if ($(window).scrollTop() > 100) {
+            $('.navbar-toggle').css('visibility', 'visible');
+            $('.navbar-fixed-top').css('background', '#fff');
+        }
+        if ($(window).scrollTop() < 50) {
+            $('.navbar-toggle').css('visibility', 'hidden');
+            $('nav').css('background', 'transparent');
+        }
+    });
+}
+
+function hideAddressBar() {
+    if (!window.location.hash) {
+        if (document.height < window.outerHeight) {
+            document.body.style.height = (window.outerHeight + 50) + 'px';
+        }
+
+        setTimeout(function() { window.scrollTo(0, 1); }, 50);
+    }
+}
+
+$(document).ready(function() {
+
+    window.addEventListener("load", function() {
+        if (!window.pageYOffset) { hideAddressBar(); } });
+    window.addEventListener("orientationchange", hideAddressBar);
+
+    changeNav();
+});
